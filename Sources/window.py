1c0131694a8f9272c039682e6037534bf796a284
AtthaphanPak <atthaphanpaksakunnee@gmail.com>
2025-07-15 17:00:10 +0700

    update

--- Sources/window.py
+++ Sources/window.py
@@ -41,41 +41,38 @@ def scan_main_serial(operation, model= "*"):
     root.mainloop()
     return result.get()
 
-def scan_sub_serial(num_serials: list):
-    if len(num_serials) == 0:
+def scan_sub_serial(sub_names: list):
+    if len(sub_names) == 0:
         return None
     
-    result = []
     def on_submit():
-        sub_serial = [entry.get().strip() for entry in entry_list]
-        if all(sub_serial):
-            result.extend(sub_serial)
+        serials = [entry.get().strip() for entry in entry_list]
+        if all(serials):  # ตรวจว่าทุกช่องมีข้อมูล
+            result.extend(serials)
             root.destroy()
         else:
-            label_error.config(text="Please fill all Sub Serial")
+            label_error.config(text="กรุณากรอกให้ครบทุกช่อง")
     def on_quit():
         print("User close program")
-        root.destroy()
-        os._exit(0)
-
+        root.quit()
     root = tk.Tk()
     root.title("Scan Sub Serials")
     root.attributes("-fullscreen", True)
     root.attributes("-topmost", True)
     root.protocol("WM_DELETE_WINDOW", lambda: None)
+    result = []
     entry_list = []
     frame = tk.Frame(root)
     frame.place(relx=0.5, rely=0.5, anchor="center")
-    tk.Label(frame, text=f"Please scan Sub Serial", font=("Helvetica", 32)).pack(pady=20)
-    for i in range(num_serials):
+    tk.Label(frame, text="Please scan all Sub Serial", font=("Helvetica", 32)).pack(pady=20)
+    for name in sub_names:
         row = tk.Frame(frame)
         row.pack(pady=10)
-        tk.Label(row, text=f"Sub Serial {i + 1}:", font=("Helvetica", 28)).pack(side="left", padx=10)
+        tk.Label(row, text=f"{name}:", font=("Helvetica", 28)).pack(side="left", padx=10)
         entry = tk.Entry(row, font=("Courier", 36), justify="center", width=20)
         entry.pack(side="left")
-        if i == 0:
-            entry.focus()
         entry_list.append(entry)
+    entry_list[0].focus()
     label_error = tk.Label(frame, text="", font=("Helvetica", 20), fg="red")
     label_error.pack()
     button_frame = tk.Frame(frame)
@@ -85,4 +82,4 @@ def scan_sub_serial(num_serials: list):
     root.mainloop()
     return result
 
-print(scan_sub_serial(2))
+print(scan_sub_serial(["BN Screw"]))
